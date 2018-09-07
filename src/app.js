b9f20bb56c35be24c259da7472ca2a007a4e7353
Christine Konrad <konradc@advisory.com>
2018-09-06 22:33:49 -0400

    Add auth

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ const appHooks = require('./app.hooks');
 
 const sequelize = require('./sequelize');
 require('dotenv').config();
+const authentication = require('./authentication');
 const app = feathers();
 
 // Load app configuration
@@ -42,6 +43,7 @@ app.configure(socketio());
 
 // Configure other middleware (see `middleware/index.js`)
 app.configure(middleware);
+app.configure(authentication);
 // Set up our services (see `services/index.js`)
 app.configure(services);
 // Configure a middleware for 404s and the error handler
--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const arcadia = require('./arcadia/arcadia.service.js');
 const client = require('./client/client.service.js');
 const account = require('./account/account.service.js');
 const bill = require('./bill/bill.service.js');
+const user = require('./user/user.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(arcadia);
   app.configure(client);
   app.configure(account);
   app.configure(bill);
+  app.configure(user);
 };
